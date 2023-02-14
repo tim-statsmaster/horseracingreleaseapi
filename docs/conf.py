6706dbdc974b5d3903e92616cf4e5b52c8c7103b
tim-statsmaster <124871915+tim-statsmaster@users.noreply.github.com>
2023-02-14 18:54:51 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -11,8 +11,12 @@
 #
 import os
 import sys
+from pathlib import Path
 
-sys.path.append(os.path.abspath('..'))
+parent = Path(__file__).parent
+parents_parent = Path(__file__).parents[1]
+
+sys.path.insert(0, parent)
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
