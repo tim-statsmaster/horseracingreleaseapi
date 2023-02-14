d18cf886802589d28c1e79ce8e2c11ba91a60fee
DESKTOP-TIM\Admin <tim_941114@hotmail.com>
2023-02-14 17:09:12 +0800

    Test

---
+++ docs/conf.py
@@ -0,0 +1,61 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'HorseRacing'
+copyright = '2023, Statsmaster Ltd.'
+author = 'Statsmaster Ltd.'
+release = '2.4.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+	'sphinxcontrib.apidoc'
+	'sphinx.ext.todo',
+	'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
+]
+extensions = [
+    'sphinxcontrib.apidoc',
+    # ...
+]
+apidoc_module_dir = '..'
+apidoc_output_dir = '.'
+apidoc_excluded_paths = ['_build', 'Thumbs.db', '.DS_Store']
+apidoc_separate_modules = True
+
+# Add any paths that contain templates here, relative to this directory.
+
+templates_path = ['_templates']
+
+source_suffix = ['.rst', '.md']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
